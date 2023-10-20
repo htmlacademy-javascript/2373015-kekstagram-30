59c10bca8c5b90f47f2b01b4809a988e0c6005b2
Keks <keks@htmlacademy.ru>
2023-10-20 18:24:41 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,4 +1,4 @@
-const PHOTO_DESCRIPTION = [
+const PHOTOS_DESCRIPTIONS = [
   'Закат над океаном.',
   'Портрет старика.',
   'Ночной город.',
@@ -6,7 +6,7 @@ const PHOTO_DESCRIPTION = [
   'Абстрактная сюрреалистичная живопись.',
 ];
 
-const USER_NAME = [
+const USERS_NAMES = [
   'Костя',
   'Родион',
   'Игорь',
@@ -14,57 +14,53 @@ const USER_NAME = [
   'Николай',
 ];
 
-const USER_COMMENTS = [
-  'Оранжевый закат над морем.',
-  'Мерцающие огоньки в городе.',
-  'Поле с разноцветными цветами.',
-  'Абстрактные формы и цвета.',
-  'Седой старик с бородой.',
+const USERS_COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
 ];
 
-const getRandomDescriptionArray = (array) => {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-};
-
-// Проверка
-// const randomDescription = getRandomDescriptionArray(PHOTO_DESCRIPTION);
-
 const getRandomInteger = (min, max) => {
-  const fraction = (max - min) * Math.random() * min;
-  return Math.round(fraction);
+  const randomInteger = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomInteger;
 };
 
-const getRandomCommentsArray = (array) => {
+const getRandomElement = (array) => {
   const randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
 };
 
-// Проверка
-// const getRandomComments = getRandomCommentsArray(USER_COMMENTS);
-
-const getRandomNameArray = (array) => {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
+const createCommentsData = () => {
+  const numComments = getRandomInteger(0, 30);
+  const comments = [];
+  for (let i = 1; i <= numComments; i++) {
+    const getRandomUserName = getRandomElement(USERS_NAMES);
+    const getRandomUserComment = getRandomElement(USERS_COMMENTS);
+    comments.push({
+      id: i,
+      avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+      message: getRandomUserComment,
+      name: getRandomUserName,
+    });
+  }
+  return comments;
 };
 
-// Проверка
-// const getRandomName = getRandomNameArray(USER_NAME);
-
-const structure = () => {
+const createPhotosData = () => {
   const array = [];
   for (let i = 1; i <= 25; i++) {
     array.push({
       id: i,
       url: `photos/${i}.jpg`,
-      description: getRandomDescriptionArray(PHOTO_DESCRIPTION),
+      description: getRandomElement(PHOTOS_DESCRIPTIONS),
       likes: getRandomInteger(15, 200),
-      comments: getRandomCommentsArray(USER_COMMENTS),
-      name: getRandomNameArray(USER_NAME),
+      comments: createCommentsData(),
     });
   }
   return array;
 };
-structure();
+createPhotosData();
 
-void (getRandomDescriptionArray, getRandomInteger, getRandomCommentsArray, getRandomNameArray);
+void (getRandomElement, getRandomInteger);
