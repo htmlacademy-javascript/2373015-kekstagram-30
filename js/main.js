74d25860c406f5e9f555a537e99c6ac347855de4
azim111 <azimaka111@gmail.com>
2023-11-01 14:47:56 +0200

    Moves thumbnails.js to the gallery directory

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 import { createPhotosData } from './data.js';
-import { renderThumbnails } from './thumbnails.js';
+import { renderThumbnails } from './gallery/thumbnails.js';
 
 renderThumbnails(createPhotosData());
