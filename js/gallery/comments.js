241fb756d6dd128fe0d053d72675f02ca768d6be
azim111 <azimaka111@gmail.com>
2023-11-03 09:32:48 +0200

    Adds a module for rendering comments

---
+++ js/gallery/comments.js
@@ -0,0 +1,22 @@
+const container = document.querySelector('.social__comments');
+const template = document.querySelector('.social__comment');
+const shownCounter = document.querySelector('.social__comment-shown-count');
+const totalCounter = document.querySelector('.social__comment-total-count');
+const loaderButton = document.querySelector('.social__comments-loader');
+
+const createComments = (commentsData) => commentsData.map(({ avatar, message, name }) => {
+  const comment = template.cloneNode(true);
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').alt = name;
+  comment.querySelector('.social__text').textContent = message;
+  return comment;
+});
+
+const renderComments = (commentsData) => {
+  container.replaceChildren(...createComments(commentsData));
+  shownCounter.textContent = commentsData.length;
+  totalCounter.textContent = commentsData.length;
+  loaderButton.classList.add('hidden');
+};
+
+export { renderComments };
