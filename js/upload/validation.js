cf6d29ff3f83187ba7c5538a1a05172457b8628d
azim111 <azimaka111@gmail.com>
2023-11-08 13:13:00 +0200

    Adds minority fixes

--- js/upload/validation.js
+++ js/upload/validation.js
@@ -13,15 +13,25 @@ const pristine = new Pristine(form, {
 
 const split = (text) => text.split(' ').filter(Boolean);
 
-pristine.addValidator(form.hashtags, (text) => split(text).every((word) => /^#[a-zа-яё0-9]+$/i.test(word), 'Неправильный хеш-тег', 1, true));
+pristine.addValidator(form.hashtags, (text) => (
+  split(text).every((word) => /^#[a-zа-яё0-9]+$/i.test(word))
+), 'Неправильный хеш-тег', 1, true);
 
-pristine.addValidator(form.hashtags, (text) => new Set(split(text.toLowerCase())).size === split(text).length, 'Повторяющийся хеш-тег', 1, true);
+pristine.addValidator(form.hashtags, (text) => (
+  new Set(split(text.toLowerCase())).size === split(text).length
+), 'Повторяющийся хеш-тег', 1, true);
 
-pristine.addValidator(form.hashtags, (text) => split(text).length <= maxHashtag, 'Превышен лимит хеш-тегов', 1, true);
+pristine.addValidator(form.hashtags, (text) => (
+  split(text).length <= maxHashtag
+), 'Превышен лимит хеш-тегов', 1, true);
 
-pristine.addValidator(form.hashtags, (text) => split(text).every((word) => word.length <= maxHashtagLength), 'Превышена максимальная длина', 1, true);
+pristine.addValidator(form.hashtags, (text) => (
+  split(text).every((word) => word.length <= maxHashtagLength)
+), 'Превышена максимальная длина', 1, true);
 
-pristine.addValidator(form.description, (text) => text.length <= maxDescriptionLength, 'Превышен лимит символов', 1, true);
+pristine.addValidator(form.description, (text) => (
+  text.length <= maxDescriptionLength
+), 'Превышен лимит символов', 1, true);
 
 const checkValidity = () => pristine.validate();
 const resetValidity = () => pristine.reset();
