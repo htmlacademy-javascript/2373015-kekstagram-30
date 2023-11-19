7945ae69241fad540241d717b5f542bd99b9911f
azim111 <azimaka111@gmail.com>
2023-11-19 22:02:10 +0200

    realizes receiving data from a server

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
-import { createPhotosData } from './data.js';
 import { renderGallery } from './gallery/main.js';
 import './upload/main.js';
+import { renderStatus, hideErrorAfterDelay } from './status.js';
+import { request } from './util.js';
 
-renderGallery(createPhotosData());
+const baseURL = 'https://30.javascript.pages.academy/kekstagram';
+
+try {
+  renderGallery(await request(`${baseURL}/data`));
+} catch {
+  renderStatus('data-error');
+  hideErrorAfterDelay();
+}
