ec249b7a764eefa16c2290c51d89f80b2e0821af
azim111 <azimaka111@gmail.com>
2023-11-19 21:55:09 +0200

    deletes unused functions

--- js/util.js
+++ js/util.js
@@ -7,14 +7,4 @@ const request = async (url, options) => {
   return response.json();
 };
 
-const getRandomInteger = (min, max) => {
-  const randomInteger = Math.floor(Math.random() * (max - min + 1) + min);
-  return randomInteger;
-};
-
-const getRandomElement = (array) => {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-};
-
-export { request, getRandomInteger, getRandomElement };
+export { request };
