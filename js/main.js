b7bbc1c3118c73891211a8ae89310ba0c328af6f
azim111 <azimaka111@gmail.com>
2023-11-19 22:45:04 +0200

    Realizes sending data to the server

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 import { renderGallery } from './gallery/main.js';
-import './upload/main.js';
+import { setSubmitDisabled } from './upload/main.js';
 import { renderStatus, hideErrorAfterDelay } from './status.js';
 import { request } from './util.js';
 
 const baseURL = 'https://30.javascript.pages.academy/kekstagram';
+
+document.addEventListener('formdata', async (event) => {
+  try {
+    setSubmitDisabled(true);
+    await request(baseURL, { method: 'post', body: event.formData });
+    renderStatus('success');
+  } catch {
+    renderStatus('error');
+  } finally {
+    setSubmitDisabled(false);
+  }
+});
 
 try {
   renderGallery(await request(`${baseURL}/data`));
