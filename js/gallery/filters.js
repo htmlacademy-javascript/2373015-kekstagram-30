883f592cf554a77c4f898ae71d725f3848302fb2
azim111 <azimaka111@gmail.com>
2023-11-21 18:33:58 +0200

    Realizes filter switching when the button is pressed

--- js/gallery/filters.js
+++ js/gallery/filters.js
@@ -1,7 +1,20 @@
 const container = document.querySelector('.img-filters');
 
+const selectFilter = (id) => {
+  container.querySelectorAll('.img-filters__button').forEach((button) => {
+    button.classList.toggle('img-filters__button--active', button.id === id);
+  });
+};
+
+const onContainerClick = (event) => {
+  if (event.target.matches('.img-filters__button')) {
+    selectFilter(event.target.id);
+  }
+};
+
 const renderFilters = () => {
   container.classList.remove('img-filters--inactive');
+  container.addEventListener('click', onContainerClick);
 };
 
 export { renderFilters };
