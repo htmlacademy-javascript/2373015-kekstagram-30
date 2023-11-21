7a969e4efd0190a0b638a950899fcc74eaaddb4b
azim111 <azimaka111@gmail.com>
2023-11-21 18:08:13 +0200

    Adds a module for managing filters

--- js/gallery/main.js
+++ js/gallery/main.js
@@ -1,11 +1,14 @@
 import { renderThumbnails } from './thumbnails.js';
 import { renderPopup } from './popup.js';
+import { renderFilters } from './filters.js';
 
 const renderGallery = (photosData) => {
+  renderFilters();
   renderThumbnails(photosData);
+
   document.addEventListener('thumbnailSelect', (event) => {
     renderPopup(event.detail);
   });
 };
 
-export { renderGallery };
+export { renderGallery, renderFilters };
