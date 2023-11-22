5bb658f544535e5e2208c313df8d0ec8c28d7d35
azim111 <azimaka111@gmail.com>
2023-11-22 14:11:52 +0200

    Reduce the frequency of gallery redrawing

--- js/gallery/main.js
+++ js/gallery/main.js
@@ -3,11 +3,11 @@ import { renderPopup } from './popup.js';
 import { renderFilters } from './filters.js';
 import { applyRandomFilter, applyDiscussedFilter } from './data.js';
 
-const renderGallery = (photosData) => {
+const renderGallery = (photosData, reduceFrequency) => {
   renderFilters();
   renderThumbnails(photosData);
 
-  document.addEventListener('filterSelect', (event) => {
+  document.addEventListener('filterSelect', reduceFrequency((event) => {
     switch (event.detail) {
       case 'filter-default':
         renderThumbnails(photosData);
@@ -19,7 +19,7 @@ const renderGallery = (photosData) => {
         renderThumbnails(applyDiscussedFilter(photosData));
         break;
     }
-  });
+  }));
 
   document.addEventListener('thumbnailSelect', (event) => {
     renderPopup(event.detail);
